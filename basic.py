161530b597bf95d276dd19a41ed9c10518cb77ae
Melonstudio31 <melonstudio31@naver.com>
2020-12-31 09:05:30 +0900

    20201231_1

---
+++ basic.py
@@ -0,0 +1,17 @@
+# print
+def ex_1():
+    print('Hello!')
+
+# variables / type
+def ex_2():
+    print(type(1))
+    print(type(3.14))
+    print(type('Hello!'))
+    print(type(True))
+
+
+
+
+if __name__ == "__main__":
+    ex_1()
+    ex_2()
