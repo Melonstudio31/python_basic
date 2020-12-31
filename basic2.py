6bec81590cb5429e97af82e635f6b7f6048d90d2
Melonstudio31 <melonstudio31@naver.com>
2020-12-31 14:48:15 +0900

    iteration

--- basic2.py
+++ basic2.py
@@ -137,13 +137,103 @@ def quiz_1():
 
 # if
 def ex_6():
-    
+    weather = input("오늘 날씨는 어때요? ")
+    if weather == '비' or weather == '눈':
+        print('우산을 챙기세요.')
+    elif weather == '미세먼지':
+        print('마스크를 챙기세요.')
+    else:
+        print('날씨가 괜찮습니다.')
 
+    temp = int(input("기온은 어때요? "))
+    if temp >= 30:
+        print('너무 더워요. 나가지 마세요.')
+    elif temp < 30 and temp >= 10:
+        print('괜찮은 날씨에요.')
+    elif temp < 10 and temp >= 0:
+        print('외투를 챙기세요.')
+    else:
+        print("너무 추워요. 나가지 마세요.")
 
+# for
+def ex_7():
+    for waiting_no in [1,2,3,4]:
+        print("대기번호 : {0}".format(waiting_no))
+
+    for i in range(1, 5):
+        print("대기번호 : " + str(i))
+
+    starbucks = ['아이언맨', '토르', '아이엠 그루트']
+    for customer in starbucks:
+        print("{0}, 커피가 준비되었습니다.".format(customer))
+
+# while
+def ex_8():
+    customer = '토르'
+    index = 5
+    while index >= 1:
+        print("{0}, 커피가 준비 되었습니다. {1} 번 남았어요.".format(customer, index))
+        index -= 1
+        if index == 0:
+            print("커피는 폐기처분 되었습니다.")
+
+    '''
+    # (무한루프)
+    while True:
+        print("{0}, 커피가 준비 되었습니다.".format(index))
+        index += 1
+    '''
+
+    customer = "토르"
+    person = "Unknown"
+    while person != customer:
+        print("{0}, 커피가 준비 되었습니다.".format(customer))
+        person = input("이름이 무엇인가요? ")
+
+# continue, break
+def ex_9():
+    absent = [2, 5]
+    no_book = [7]
+    for student in range(1, 11):
+        if student in absent:
+            continue
+        elif student in no_book:
+            print("수업 그만. {0}은 교무실로 따라와.".format(student))
+            break
+        print("{0}, 책을 읽어봐.".format(student))
+
+# one line for
+def ex_10():
+    students = [1,2,3,4,5]
+    print(students)
+    students = [i+100 for i in students]
+    print(students)
+
+    students = ['Iron man', 'Thor', 'I am groot']
+    students = [len(i)for i in students]
+    print(students)
+
+    students = ['Iron man', 'Thor', 'I am groot']
+    students = [i.upper() for i in students]
+    print(students)
+
+# quiz_2
+def quiz_2():
+    count = 0
+
+    for i in range(1, 51):
+        time = randrange(5, 51)
+        if 5 <= time and time <= 15:
+            print("[0] {0}번째 손님 (소요시간 : {1}분)".format(i, time))
+            count += 1
+        else:
+            print("[ ] {0}번째 손님 (소요시간 : {1}분)".format(i, time))
+
+    print("총 탑승 승객 :" + count)
 
 
 # execute example
 if __name__ == "__main__":
-    # ex_5()             # ex_1 ~ ex_15
-    quiz_1()            # quiz_1 ~ quiz_3
+    #ex_10()             # ex_1 ~ ex_10
+    #quiz_2()            # quiz_1 ~ quiz_2
     pass                  # pass는 지우고 실행
